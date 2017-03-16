dfe3f6c566b21c29f19475c0bbd2e3f4cd01708e
Alykhan Kanji <github@alykhan.com>
2017-03-16 00:30:37 -0700

    Configure ESLint
    * Run automatically via Travis CI before tests

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ Work in progress!
 
 ```
 npm install
+npm run lint
 npm test
 ```
 
--- test/routes-specs.js
+++ test/routes-specs.js
@@ -6,14 +6,12 @@ const request = require('supertest')
 const http = require('http')
 const server = require('../lib/server.js')
 
-
 chai.should()
 chai.use(chaiAsPromised)
 
-const koaRequest = request(http.createServer(server.callback()));
+const koaRequest = request(http.createServer(server.callback()))
 
 describe('routes', () => {
-
   context('GET /', () => {
     it('should respond 200', async () => {
       await koaRequest
