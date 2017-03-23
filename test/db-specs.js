0b8464b26bb1cc18906e2ce251a3b77576ff0c5d
Nidhi <nidhidhammani@gmail.com>
2017-03-22 22:49:59 -0700

    Adding db models, specs and index

---
+++ test/db-specs.js
@@ -0,0 +1,18 @@
+/* eslint-env mocha */
+
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+const model = require('../lib/models')
+
+chai.should()
+chai.use(chaiAsPromised)
+
+describe('Team model', () => {
+  before(() => {
+    return model.db.sequelize.sync();
+  })
+  it('should save team', async () => {
+    let team = await model.db.team.create({id: 1, name: 'abc'})
+    team['name'].should.equal('abc')
+  })
+})
