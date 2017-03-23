73a668ff55bf62d2cfd02f276f24e0eaff73e844
Nidhi <nidhidhammani@gmail.com>
2017-03-22 22:56:41 -0700

    Linting linting linting

--- test/db-specs.js
+++ test/db-specs.js
@@ -9,7 +9,7 @@ chai.use(chaiAsPromised)
 
 describe('Team model', () => {
   before(() => {
-    return model.db.sequelize.sync();
+    return model.db.sequelize.sync()
   })
   it('should save team', async () => {
     let team = await model.db.team.create({id: 1, name: 'abc'})
