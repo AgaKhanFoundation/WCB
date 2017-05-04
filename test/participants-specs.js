1253720692850da264d6c72bedd81d212e534758
Alykhan Kanji <github@alykhan.com>
2017-05-04 00:25:57 -0700

    Fix issues with participants endpoint and test

---
+++ test/participants-specs.js
@@ -0,0 +1,62 @@
+/* eslint-env mocha */
+
+const koaRequest = require('./routes-specs').koaRequest
+const model = require('./routes-specs').model
+
+beforeEach(function syncDB () {
+  return model.db.sequelize.sync({force: true})
+})
+
+describe('participants', () => {
+  context('GET /participants/:id', () => {
+    it('should return 204 if no participant with fbid=id', async () => {
+      await koaRequest
+        .get('/participants/1')
+        .expect(204)
+    })
+    it('should return participant with fbid=id', async () => {
+      let participant = await model.db.participants.create({fbid: 'p1'})
+      await koaRequest
+        .get('/participants/' + participant.fbid)
+        .expect(200)
+        .then(response => {
+          response.body.fbid.should.equal(participant.fbid)
+        })
+    })
+  })
+
+  context('POST /participants', () => {
+    it('should create participant with fbid=id', async () => {
+      let fbid = 'p1'
+      await koaRequest
+        .post('/participants')
+        .send({id: fbid})
+        .expect(201)
+        .then(response => {
+          response.body.fbid.should.equal(fbid)
+        })
+    })
+    it('should return 409 if participant fbid conflict', async () => {
+      let fbid = 'p2'
+      let p2 = await model.db.participants.create({fbid})
+      await koaRequest
+        .post('/participants')
+        .send({id: p2.fbid})
+        .expect(409, `Participant with fbid="${p2.fbid}" already exists!`)
+    })
+  })
+
+  context('DELETE /participants/:id', () => {
+    it('should delete participant with fbid=id', async () => {
+      let participant = await model.db.participants.create({fbid: 'p1'})
+      await koaRequest
+        .del('/participants/' + participant.fbid)
+        .expect(204)
+    })
+    it('should return 400 if no participant with fbid=id', async () => {
+      await koaRequest
+        .del('/participants/' + 0)
+        .expect(400)
+    })
+  })
+})
