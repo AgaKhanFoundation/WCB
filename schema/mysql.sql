e0bedc24c5d4384841d7982015bab53e81831e0b
Alykhan Kanji <github@alykhan.com>
2017-05-03 23:34:36 -0700

    Ensure parity between lib/models/ and schema/
    * Fix typos in table names
    * Disable ORM timestamps
    * Add lib/models/cause.js

--- schema/mysql.sql
+++ schema/mysql.sql
@@ -2,51 +2,51 @@ CREATE DATABASE wcb;
 use wcb;
 
 CREATE TABLE `team` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL UNIQUE,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `participants` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `fbid` VARCHAR(255) NOT NULL UNIQUE,
-  `team` INT NOT NULL,
-  `event` INT NOT NULL,
-  `datasource` INT NOT NULL,
+  `team` INT,
+  `event` INT,
+  `datasource` INT,
   `preferred_cause` INT,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `achievements` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `team` INT NOT NULL,
   `achievement` INT NOT NULL,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `achievement` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL UNIQUE,
   `distance` INT NOT NULL,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `donors` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `donor` INT NOT NULL,
   `participant` INT NOT NULL,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `donor` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL UNIQUE,
   `luminate_id` VARCHAR(255) NOT NULL UNIQUE,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `records` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `participant` INT NOT NULL,
   `date` DATE NOT NULL,
   `distance` INT NOT NULL,
@@ -55,13 +55,13 @@ CREATE TABLE `records` (
 );
 
 CREATE TABLE `sources` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL UNIQUE,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `events` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   `description` TEXT NOT NULL,
   `start_date` DATE NOT NULL,
@@ -74,28 +74,28 @@ CREATE TABLE `events` (
   PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `localities` (
-  `id` INT NOT NULL,
+CREATE TABLE `locality` (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL UNIQUE,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `sponsors` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `sponsor` INT NOT NULL,
   `participant` INT NOT NULL,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `sponsor` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL UNIQUE,
   `luminate_id` VARCHAR(255) NOT NULL UNIQUE,
   PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `causes` (
-  `id` INT NOT NULL,
+CREATE TABLE `cause` (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL UNIQUE,
   PRIMARY KEY (`id`)
 );
@@ -106,7 +106,7 @@ ALTER TABLE `participants` ADD CONSTRAINT `participants_fk1` FOREIGN KEY (`event
 
 ALTER TABLE `participants` ADD CONSTRAINT `participants_fk2` FOREIGN KEY (`datasource`) REFERENCES `sources`(`id`);
 
-ALTER TABLE `participants` ADD CONSTRAINT `participants_fk3` FOREIGN KEY (`preferred_cause`) REFERENCES `causes`(`id`);
+ALTER TABLE `participants` ADD CONSTRAINT `participants_fk3` FOREIGN KEY (`preferred_cause`) REFERENCES `cause`(`id`);
 
 ALTER TABLE `achievements` ADD CONSTRAINT `achievements_fk0` FOREIGN KEY (`team`) REFERENCES `team`(`id`);
 
@@ -120,11 +120,10 @@ ALTER TABLE `records` ADD CONSTRAINT `records_fk0` FOREIGN KEY (`participant`) R
 
 ALTER TABLE `records` ADD CONSTRAINT `records_fk1` FOREIGN KEY (`source`) REFERENCES `sources`(`id`);
 
-ALTER TABLE `events` ADD CONSTRAINT `events_fk0` FOREIGN KEY (`locality`) REFERENCES `localities`(`id`);
+ALTER TABLE `events` ADD CONSTRAINT `events_fk0` FOREIGN KEY (`locality`) REFERENCES `locality`(`id`);
 
-ALTER TABLE `events` ADD CONSTRAINT `events_fk1` FOREIGN KEY (`cause`) REFERENCES `causes`(`id`);
+ALTER TABLE `events` ADD CONSTRAINT `events_fk1` FOREIGN KEY (`cause`) REFERENCES `cause`(`id`);
 
 ALTER TABLE `sponsors` ADD CONSTRAINT `sponsors_fk0` FOREIGN KEY (`sponsor`) REFERENCES `sponsor`(`id`);
 
 ALTER TABLE `sponsors` ADD CONSTRAINT `sponsors_fk1` FOREIGN KEY (`participant`) REFERENCES `participants`(`id`);
-
