1d6777e53d269892974509ce2f68089c407c44fe
Alykhan Kanji <github@alykhan.com>
2017-05-20 18:48:44 -0700

    Add tests for events endpoints
    * Use chai-moment for testing datetime equality
    * Add uniqueness verification for name field in /POST events

--- test/routes-specs.js
+++ test/routes-specs.js
@@ -1,14 +1,14 @@
 /* eslint-env mocha */
 
 const chai = require('chai')
-const chaiAsPromised = require('chai-as-promised')
 const request = require('supertest')
 const http = require('http')
 const server = require('../lib/server.js')
 const models = require('../lib/models')
 
 chai.should()
-chai.use(chaiAsPromised)
+chai.use(require('chai-as-promised'))
+chai.use(require('chai-moment'))
 
 const koaRequest = request(http.createServer(server.callback()))
 
