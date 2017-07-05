887fbcd862ada24081a2fb9cade64e1b98b955ee
Moiz <moizjv@gmail.com>
2017-07-04 21:11:51 -0700

    Fix lint

--- config/staging-migrations.js
+++ config/staging-migrations.js
@@ -1,10 +1,10 @@
 module.exports = {
-  "migrations-path": "lib/migrations",
-  "models-path": "lib/models",
-  "database": "wcb",
-  "host": "localhost",
-  "dialect": "mysql",
-  "port": 3306,
-  "username": process.env.username,
-  "password": process.env.password
+  'migrations-path': 'lib/migrations',
+  'models-path': 'lib/models',
+  'database': 'wcb',
+  'host': 'localhost',
+  'dialect': 'mysql',
+  'port': 3306,
+  'username': process.env.username,
+  'password': process.env.password
 }
