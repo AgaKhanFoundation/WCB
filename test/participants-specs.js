4d3ad236636048acbbd7b0c9777ee4cedf91e3a6
Alykhan Kanji <github@alykhan.com>
2019-10-26 18:47:53 -0700

    Accept 'registered' parameter for participant

--- test/participants-specs.js
+++ test/participants-specs.js
@@ -21,11 +21,12 @@ describe('participants', () => {
         .expect(200)
         .then(response => {
           response.body.fbid.should.equal(participant.fbid)
+          response.body.registered.should.equal(participant.registered)
         })
     })
     it('should return participant with fbid=fbid including team, participants, and achievements', async () => {
       let t1 = await models.db.team.create({name: 't1'})
-      let p1 = await models.db.participant.create({fbid: 'p1', team_id: t1.id})
+      let p1 = await models.db.participant.create({fbid: 'p1', registered: true, team_id: t1.id})
       let a1 = await models.db.achievement.create({name: 'a1', distance: 1})
       await models.db.team_achievement.create({team_id: t1.id, achievement_id: a1.id})
       await koaRequest
@@ -33,6 +34,7 @@ describe('participants', () => {
         .expect(200)
         .then(response => {
           response.body.fbid.should.equal(p1.fbid)
+          response.body.registered.should.equal(p1.registered)
           response.body.team_id.should.equal(t1.id)
           response.body.team.name.should.equal(t1.name)
           response.body.team.participants[0].fbid.should.equal(p1.fbid)
@@ -73,14 +75,16 @@ describe('participants', () => {
   })
 
   context('POST /participants', () => {
-    it('should create participant with fbid=fbid', async () => {
+    it('should create participant with fbid=fbid and registered=registered', async () => {
       let fbid = 'p1'
+      let registered = true
       await koaRequest
         .post('/participants')
-        .send({fbid})
+        .send({fbid, registered})
         .expect(201)
         .then(response => {
           response.body.fbid.should.equal(fbid)
+          response.body.registered.should.equal(registered)
         })
     })
     it('should return 409 if participant fbid conflict', async () => {
@@ -89,6 +93,18 @@ describe('participants', () => {
       await koaRequest
         .post('/participants')
         .send({fbid})
+        .expect(409, {'error': {
+          'code': 409,
+          'message': `participant with fbid="${p2.fbid}" already exists`
+        }})
+    })
+    it('should return 409 if participant fbid conflict, regardless of registered status', async () => {
+      let fbid = 'p2'
+      let registered = false
+      let p2 = await models.db.participant.create({fbid, registered: true})
+      await koaRequest
+        .post('/participants')
+        .send({fbid, registered})
         .expect(409, {'error': {
           'code': 409,
           'message': `participant with fbid="${p2.fbid}" already exists`
@@ -120,6 +136,13 @@ describe('participants', () => {
           'code': 400,
           'message': 'SQLITE_CONSTRAINT: FOREIGN KEY constraint failed'
         }})
+    })
+    it('should set registered for participant with fbid=fbid', async () => {
+      let p1 = await models.db.participant.create({fbid: 'p1'})
+      await koaRequest
+        .patch('/participants/' + p1.fbid)
+        .send({registered: true})
+        .expect(200)
     })
   })
 
