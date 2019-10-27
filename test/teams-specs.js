47f739caa381fef2992d2897745dfa604167b760
Alykhan Kanji <github@alykhan.com>
2019-10-27 14:02:43 -0700

    Create leaderboard endpoint to serve all stats

--- test/teams-specs.js
+++ test/teams-specs.js
@@ -7,6 +7,20 @@ beforeEach(function syncDB () {
   return models.db.sequelize.sync({force: true})
 })
 
+async function createEvent (name) {
+  return models.db.event.create({
+    name: name,
+    image: 'i1',
+    description: 'event 1',
+    start_date: '2017-02-01T00:00:00Z',
+    end_date: '2017-12-31T00:00:00Z',
+    team_limit: 10,
+    team_building_start: '2017-01-01T00:00:00Z',
+    team_building_end: '2017-01-31T00:00:00Z',
+    default_steps: 10000
+  })
+}
+
 describe('teams', () => {
   context('GET /teams', () => {
     it('should return teams', async () => {
@@ -57,6 +71,195 @@ describe('teams', () => {
     })
   })
 
+  context('GET /teams/stats/:event', () => {
+    it('should return 204 if no event with id=id', async () => {
+      await koaRequest
+        .get('/teams/stats/1')
+        .expect(204)
+    })
+    it('should return empty array if no teams', async () => {
+      await createEvent('e1')
+      await koaRequest
+        .get('/teams/stats/1')
+        .expect(200)
+        .then(response => {
+          response.body.should.deep.equal([])
+        })
+    })
+    it('should return zero distance and zero commitment for team with no records or commitments', async () => {
+      let e1 = await createEvent('e1')
+      let t1 = await models.db.team.create({name: 't1', image: 'i1'})
+      await models.db.participant.create({fbid: 'p1', team_id: t1.id})
+      await koaRequest
+        .get('/teams/stats/' + e1.id)
+        .expect(200)
+        .then(response => {
+          response.body[0].id.should.equal(t1.id)
+          response.body[0].name.should.equal(t1.name)
+          response.body[0].image.should.equal(t1.image)
+          response.body[0].hidden.should.equal(t1.hidden)
+          response.body[0].distance.should.equal(0)
+          response.body[0].commitment.should.equal(0)
+        })
+    })
+    it('should return participant distance for team with single participant', async () => {
+      let e1 = await createEvent('e1')
+      let t1 = await models.db.team.create({name: 't1'})
+      let p1 = await models.db.participant.create({fbid: 'p1', team_id: t1.id})
+      let s1 = await models.db.source.create({name: 's1'})
+      let r1 = await models.db.record.create({
+        date: '2017-02-01T00:00:00Z',
+        distance: 10,
+        participant_id: p1.id,
+        source_id: s1.id
+      })
+      let r2 = await models.db.record.create({
+        date: '2017-02-02T00:00:00Z',
+        distance: 20,
+        participant_id: p1.id,
+        source_id: s1.id
+      })
+      await koaRequest
+        .get('/teams/stats/' + e1.id)
+        .expect(200)
+        .then(response => {
+          response.body[0].distance.should.equal(r1.distance + r2.distance)
+          response.body[0].commitment.should.equal(0)
+        })
+    })
+    it('should return only participant distance recorded during event challenge period', async () => {
+      let e1 = await createEvent('e1')
+      let t1 = await models.db.team.create({name: 't1'})
+      let p1 = await models.db.participant.create({fbid: 'p1', team_id: t1.id})
+      let s1 = await models.db.source.create({name: 's1'})
+      await models.db.record.create({
+        date: '2017-01-01T00:00:00Z',
+        distance: 10,
+        participant_id: p1.id,
+        source_id: s1.id
+      })
+      let r2 = await models.db.record.create({
+        date: '2017-02-01T00:00:00Z',
+        distance: 20,
+        participant_id: p1.id,
+        source_id: s1.id
+      })
+      await koaRequest
+        .get('/teams/stats/' + e1.id)
+        .expect(200)
+        .then(response => {
+          response.body[0].distance.should.equal(r2.distance)
+          response.body[0].commitment.should.equal(0)
+        })
+    })
+    it('should return participant commitment for team with single participant', async () => {
+      let e1 = await createEvent('e1')
+      let t1 = await models.db.team.create({name: 't1'})
+      let p1 = await models.db.participant.create({fbid: 'p1', team_id: t1.id})
+      let c1 = await models.db.participant_event.create({participant_id: p1.id, event_id: e1.id, commitment: 100000})
+      await koaRequest
+        .get('/teams/stats/' + e1.id)
+        .expect(200)
+        .then(response => {
+          response.body[0].distance.should.equal(0)
+          response.body[0].commitment.should.equal(c1.commitment)
+        })
+    })
+    it('should return only participant commitment for specified event', async () => {
+      let e1 = await createEvent('e1')
+      let e2 = await createEvent('e2')
+      let t1 = await models.db.team.create({name: 't1'})
+      let p1 = await models.db.participant.create({fbid: 'p1', team_id: t1.id})
+      let c1 = await models.db.participant_event.create({participant_id: p1.id, event_id: e1.id, commitment: 100000})
+      await models.db.participant_event.create({participant_id: p1.id, event_id: e2.id, commitment: 200000})
+      await koaRequest
+        .get('/teams/stats/' + e1.id)
+        .expect(200)
+        .then(response => {
+          response.body[0].distance.should.equal(0)
+          response.body[0].commitment.should.equal(c1.commitment)
+        })
+    })
+    it('should return participant distance and commitment for team with multiple participants', async () => {
+      let e1 = await createEvent('e1')
+      let t1 = await models.db.team.create({name: 't1'})
+      let p1 = await models.db.participant.create({fbid: 'p1', team_id: t1.id})
+      let p2 = await models.db.participant.create({fbid: 'p2', team_id: t1.id})
+      let s1 = await models.db.source.create({name: 's1'})
+      let r1 = await models.db.record.create({
+        date: '2017-02-01T00:00:00Z',
+        distance: 10,
+        participant_id: p1.id,
+        source_id: s1.id
+      })
+      let r2 = await models.db.record.create({
+        date: '2017-02-02T00:00:00Z',
+        distance: 20,
+        participant_id: p2.id,
+        source_id: s1.id
+      })
+      let c1 = await models.db.participant_event.create({participant_id: p1.id, event_id: e1.id, commitment: 10000})
+      let c2 = await models.db.participant_event.create({participant_id: p2.id, event_id: e1.id, commitment: 20000})
+      await koaRequest
+        .get('/teams/stats/' + e1.id)
+        .expect(200)
+        .then(response => {
+          response.body[0].distance.should.equal(r1.distance + r2.distance)
+          response.body[0].commitment.should.equal(c1.commitment + c2.commitment)
+        })
+    })
+    it('should return participant distance and commitment for multiple teams', async () => {
+      let e1 = await createEvent('e1')
+      let t1 = await models.db.team.create({name: 't1'})
+      let t2 = await models.db.team.create({name: 't2'})
+      let p1 = await models.db.participant.create({fbid: 'p1', team_id: t1.id})
+      let p2 = await models.db.participant.create({fbid: 'p2', team_id: t1.id})
+      let p3 = await models.db.participant.create({fbid: 'p3', team_id: t2.id})
+      let p4 = await models.db.participant.create({fbid: 'p4', team_id: t2.id})
+      let s1 = await models.db.source.create({name: 's1'})
+      let r1 = await models.db.record.create({
+        date: '2017-02-01T00:00:00Z',
+        distance: 10,
+        participant_id: p1.id,
+        source_id: s1.id
+      })
+      let r2 = await models.db.record.create({
+        date: '2017-02-01T00:00:00Z',
+        distance: 20,
+        participant_id: p2.id,
+        source_id: s1.id
+      })
+      let r3 = await models.db.record.create({
+        date: '2017-02-01T00:00:00Z',
+        distance: 30,
+        participant_id: p3.id,
+        source_id: s1.id
+      })
+      let r4 = await models.db.record.create({
+        date: '2017-02-01T00:00:00Z',
+        distance: 40,
+        participant_id: p4.id,
+        source_id: s1.id
+      })
+      let c1 = await models.db.participant_event.create({participant_id: p1.id, event_id: e1.id, commitment: 10000})
+      let c2 = await models.db.participant_event.create({participant_id: p2.id, event_id: e1.id, commitment: 20000})
+      let c3 = await models.db.participant_event.create({participant_id: p3.id, event_id: e1.id, commitment: 30000})
+      let c4 = await models.db.participant_event.create({participant_id: p4.id, event_id: e1.id, commitment: 40000})
+      await koaRequest
+        .get('/teams/stats/' + e1.id)
+        .expect(200)
+        .then(response => {
+          response.body.should.have.lengthOf(2)
+          response.body[0].id.should.equal(t1.id)
+          response.body[0].distance.should.equal(r1.distance + r2.distance)
+          response.body[0].commitment.should.equal(c1.commitment + c2.commitment)
+          response.body[1].id.should.equal(t2.id)
+          response.body[1].distance.should.equal(r3.distance + r4.distance)
+          response.body[1].commitment.should.equal(c3.commitment + c4.commitment)
+        })
+    })
+  })
+
   context('GET /teams/:id', () => {
     it('should return 204 if no team with id=id', async () => {
       await koaRequest
@@ -99,38 +302,6 @@ describe('teams', () => {
           response.body.achievements[0].sequence.should.equal(a1.sequence)
           response.body.achievements[0].name.should.equal(a1.name)
           response.body.achievements[0].distance.should.equal(a1.distance)
-        })
-    })
-  })
-
-  context('GET /teams/:id/stats', () => {
-    it('should return 204 if no team with id=id', async () => {
-      await koaRequest
-        .get('/teams/1/stats')
-        .expect(204)
-    })
-    it('should return sum of distances for participants in team with id=id', async () => {
-      let t1 = await models.db.team.create({name: 't1'})
-      let p1 = await models.db.participant.create({fbid: 'p1', team_id: t1.id})
-      let p2 = await models.db.participant.create({fbid: 'p2', team_id: t1.id})
-      let s1 = await models.db.source.create({name: 's1'})
-      let r1 = await models.db.record.create({
-        date: '2017-01-01T00:00:00Z',
-        distance: 6,
-        participant_id: p1.id,
-        source_id: s1.id
-      })
-      let r2 = await models.db.record.create({
-        date: '2017-01-02T00:00:00Z',
-        distance: 12,
-        participant_id: p2.id,
-        source_id: s1.id
-      })
-      await koaRequest
-        .get('/teams/' + t1.id + '/stats')
-        .expect(200)
-        .then(response => {
-          response.body.distance.should.equal(r1.distance + r2.distance)
         })
     })
   })
